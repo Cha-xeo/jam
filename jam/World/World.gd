b1dc9c03c0e0fd6f79ed6ac3b119323fff86b99e
Cha-xeo <75042650+Cha-xeo@users.noreply.github.com>
2021-02-14 17:24:14 +0100

    gameOverScreen

--- jam/World/World.gd
+++ jam/World/World.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+onready var gameOver = $CanvasLayer/GameOverScreen
 func _ready():
-	pass # Replace with function body.
+	gameOver.hide()
+
+func _process(delta):
+	if Input.is_action_pressed("ui_cancel"): #escape à changer par la conditio de defaite
+		gameOver.show()
+
+func _on_GameRestart_pressed():
+	print("restart the game")
+	gameOver.hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Quit_pressed():
+	queue_free()
+	get_tree().quit()
